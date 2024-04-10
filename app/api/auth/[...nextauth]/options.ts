81bdf06477b8527ab92dc55c8774d8fd8bd2e76f
Ivakel <ivakeledlabom88@gmail.com>
2024-04-10 17:50:20 +0200

    update Lab1-15

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,56 @@
+import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import { NextAuthOptions } from "next-auth";
+
+
+const getGoogleClientId = () => {
+    const clientId = process.env.GOOGLE_CLIENT_ID;
+    if (!clientId) {
+      throw new Error("Google client ID not found");
+    }
+  
+    return clientId;
+  };
+const getGoogleClientSecret = () => {
+    const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+    if (!clientSecret) {
+      throw new Error("Google client secret not found");
+    }
+  
+    return clientSecret;
+  };
+  
+export const options: NextAuthOptions = {
+  providers: [
+  GoogleProvider({
+    clientId: getGoogleClientId(),
+    clientSecret: getGoogleClientSecret()
+  }),
+  CredentialsProvider({
+      
+    name: "Credentials",
+
+    credentials: {
+      username: {
+          label: "Username:",
+          type: "text",
+          placeholder: "your-cool-username"
+      },
+      password: {
+          label: "Password:",
+          type: "password",
+          placeholder: "your-awesome-password"
+      }
+  },
+    async authorize(credentials,) {
+      
+      const user = {id: "42", name: "Iva", password: "nextauth"};
+
+      if (credentials?.username === user.name && credentials?.password === user.password) {
+          return user
+      } else {
+          return null
+      }
+    },
+  }),
+]};
