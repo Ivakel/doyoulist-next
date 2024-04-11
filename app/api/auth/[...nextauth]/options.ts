6c3aed12bf8278ba87f0364c0c87157eebc79fe2
Ivakel <ivakeledlabom88@gmail.com>
2024-04-11 17:24:35 +0200

    udate my-pc

--- app/api/auth/[...nextauth]/options.ts
+++ app/api/auth/[...nextauth]/options.ts
@@ -2,55 +2,59 @@ import GoogleProvider from "next-auth/providers/google";
 import CredentialsProvider from "next-auth/providers/credentials";
 import { NextAuthOptions } from "next-auth";
 
-
 const getGoogleClientId = () => {
-    const clientId = process.env.GOOGLE_CLIENT_ID;
-    if (!clientId) {
-      throw new Error("Google client ID not found");
-    }
-  
-    return clientId;
-  };
+  const clientId = process.env.GOOGLE_CLIENT_ID;
+  if (!clientId) {
+    throw new Error("Google client ID not found");
+  }
+
+  return clientId;
+};
 const getGoogleClientSecret = () => {
-    const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
-    if (!clientSecret) {
-      throw new Error("Google client secret not found");
-    }
-  
-    return clientSecret;
-  };
-  
+  const clientSecret = process.env.GOOGLE_CLIENT_SECRET;
+  if (!clientSecret) {
+    throw new Error("Google client secret not found");
+  }
+
+  return clientSecret;
+};
+
 export const options: NextAuthOptions = {
   providers: [
-  GoogleProvider({
-    clientId: getGoogleClientId(),
-    clientSecret: getGoogleClientSecret()
-  }),
-  CredentialsProvider({
-      
-    name: "Credentials",
+    GoogleProvider({
+      clientId: getGoogleClientId(),
+      clientSecret: getGoogleClientSecret(),
+    }),
+    CredentialsProvider({
+      name: "Credentials",
 
-    credentials: {
-      username: {
+      credentials: {
+        username: {
           label: "Username:",
           type: "text",
-          placeholder: "your-cool-username"
-      },
-      password: {
+          placeholder: "your-cool-username",
+        },
+        password: {
           label: "Password:",
           type: "password",
-          placeholder: "your-awesome-password"
-      }
-  },
-    async authorize(credentials,) {
-      
-      const user = {id: "42", name: "Iva", password: "nextauth"};
+          placeholder: "your-awesome-password",
+        },
+      },
+      async authorize(credentials) {
+        const user = { id: "42", name: "Iva", password: "nextauth" };
 
-      if (credentials?.username === user.name && credentials?.password === user.password) {
-          return user
-      } else {
-          return null
-      }
-    },
-  }),
-]};
+        if (
+          credentials?.username === user.name &&
+          credentials?.password === user.password
+        ) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+  pages: {
+    signIn: "/login",
+  },
+};
--- app/login/loginForm.tsx
+++ app/login/loginForm.tsx
@@ -14,29 +14,29 @@ import {
 import { Input } from "@/components/ui/input";
 import Image from "next/image";
 //Import images
-import AppLogo from "@/public/svg/logo.svg"
-import Google from "@/public/svg/google 2.svg"
+import AppLogo from "@/public/svg/logo.svg";
+import Google from "@/public/svg/google 2.svg";
 import RegisterOptionDivider from "@/components/registerOptionDivider";
-import TermsAgree from "@/components/termsAgree";
 import Link from "next/link";
 
 export default function LoginForm() {
-
   const formSchema = z.object({
     email: z
       .string()
       .min(1, { message: "This field has to be filled." })
       .email("This is not a valid email."),
-    password: z.string().min(8, {message: "Password must be atleast 8 characters"}).max(20, {message: "Password must be atmost 20 characters"})
+    password: z
+      .string()
+      .min(8, { message: "Password must be atleast 8 characters" })
+      .max(20, { message: "Password must be atmost 20 characters" }),
   });
   const form = useForm<z.infer<typeof formSchema>>({
     resolver: zodResolver(formSchema),
     defaultValues: {
       email: "",
-      password: ""
+      password: "",
     },
   });
-
 
   function onSubmit(values: z.infer<typeof formSchema>) {
     // Do something with the form values.
@@ -44,49 +44,75 @@ export default function LoginForm() {
     console.log(values);
   }
   return (
-    <section className="w-full h-full flex flex-col justify-center items-center relative px-5 pt-60">
+    <section className="w-full h-full flex flex-col justify-center items-center relative px-5 sm:pt-40">
       <div className="absolute h-20 w-36 top-5 left-5">
-      <Image className="" src={AppLogo} fill={true} priority alt="application logo" />
+        <Image
+          className=""
+          src={AppLogo}
+          fill={true}
+          priority
+          alt="application logo"
+        />
       </div>
       <Form {...form}>
-      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6 xl:w-96">
-        <h1 className="text-2xl text-center font-medium">Log in to <span className="text-[#575293] font-bold">Orderdly</span></h1>
-        <Button className="flex gap-4 bg-[#F8FAFC] hover:bg-[#e8ebee] text-black text-base font-semibold w-full">
-          <Image src={Google} width={20} height={20} alt="google logo"/>
-          <span className="text-sm">Continue with google</span>
-        </Button>
-        <RegisterOptionDivider/>
-        <FormField
-          control={form.control}
-          name="email"
-          render={({ field }) => (
-            <FormItem >
-              <FormControl className="bg-[#F8FAFC]">
-                <Input placeholder="Email" {...field} className="focus-visible:ring-0"/>
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-        
-          control={form.control}
-          name="password"
-          render={({ field }) => (
-            <FormItem>
-              
-              <FormControl className="bg-[#F8FAFC]">
-                <Input placeholder="Password" {...field} className="focus-visible:ring-0" />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type="submit" className="w-full bg-[#8C83C9] hover:bg-[#a398e9]">Log in</Button>
-      </form>
-      <h3 className="pt-6 text-sm">{"Don't"} have account? <Link href={"/"} className="text-[#2563EB]">Create account</Link></h3>
-    </Form>
+        <form
+          onSubmit={form.handleSubmit(onSubmit)}
+          className="space-y-6 xl:w-96"
+        >
+          <h1 className="text-2xl text-center font-medium">
+            Log in to <span className="text-[#575293] font-bold">Orderdly</span>
+          </h1>
+          <Button className="flex gap-4 bg-[#F8FAFC] hover:bg-[#e8ebee] text-black text-base font-semibold w-full">
+            <Image src={Google} width={20} height={20} alt="google logo" />
+            <span className="text-sm">Continue with google</span>
+          </Button>
+          <RegisterOptionDivider />
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl className="bg-[#F8FAFC]">
+                  <Input
+                    placeholder="Email"
+                    {...field}
+                    className="focus-visible:ring-0"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <FormField
+            control={form.control}
+            name="password"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl className="bg-[#F8FAFC]">
+                  <Input
+                    placeholder="Password"
+                    {...field}
+                    className="focus-visible:ring-0"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <Button
+            type="submit"
+            className="w-full bg-[#8C83C9] hover:bg-[#a398e9]"
+          >
+            Log in
+          </Button>
+        </form>
+        <h3 className="pt-6 text-sm">
+          {"Don't"} have account?{" "}
+          <Link href={"/register"} className="text-[#2563EB]">
+            Create account
+          </Link>
+        </h3>
+      </Form>
     </section>
   );
 }
-
