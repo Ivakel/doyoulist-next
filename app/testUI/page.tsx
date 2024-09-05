01799e1daa85a8f36878340e42893c764b35ea19
Ivakele <ivakeledlabom88@gmail.com>
2024-09-05 20:47:29 +0200

    fix

--- app/testUI/page.tsx
+++
@@ -1,9 +0,0 @@
-import { ScrollAreaHorizontalDemo } from "@/components/scrollSelectedDays"
-
-export default function page() {
-    return (
-        <div className="m-auto h-full w-full">
-            <ScrollAreaHorizontalDemo />
-        </div>
-    )
-}
