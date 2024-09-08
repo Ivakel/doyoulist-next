94d77307217f8d9b98da9835e24357889a2f76a6
Ivakele <ivakeledlabom88@gmail.com>
2024-09-08 18:01:20 +0200

    more fix

--- db/supabase/client.ts
+++
@@ -1,6 +0,0 @@
-import { env } from "@/env"
-import { createClient } from "@supabase/supabase-js"
-
-const supabase = createClient(env.SUPABASE_URL, env.SUPABASE_ANON_KEY)
-
-export default supabase
